745c1cd06389138c5518d4e863f2b4952855bd90
versorspace <resoaxes@gmail.com>
2022-10-26 05:20:46 +0530

    adding devshells

--- flake.nix
+++ flake.nix
@@ -3,8 +3,18 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.dwm = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    packages.x86_64-linux.dwm = pkgs.callPackage ./default.nix {};
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        xorg.libX11
+	xorg.libXinerama
+	xorg.libXft
+      ];
+    };
   };
 }
