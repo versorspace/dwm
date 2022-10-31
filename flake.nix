69960c2ae2709c1d3b602d7f6efc1e70e49b0417
versorspace <resoaxes@gmail.com>
2022-10-31 17:41:04 +0530

    corrected flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,24 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in
   {
-    packages.x86_64-linux.dwm = pkgs.callPackage ./default.nix {};
+    packages.x86_64-linux.dwm = pkgs.callPackage (
+      { lib, stdenv, fetchurl, libX11, libXinerama, libXft }:
+        stdenv.mkDerivation rec {
+          pname = "dwm";
+          version = "6.3";
+        
+          src = ./.;
+        
+          buildInputs = [ libX11 libXinerama libXft ];
+        
+          makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+
+	  installFlags = [ "PREFIX=$(out)" ];
+	}
+      ) {};
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.dwm;
+
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [
         xorg.libX11
